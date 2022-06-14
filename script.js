4e74d3987aed88f3d07c67e110a6d9bbc54d4cb9
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-14 16:41:54 +0200

    finished project

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const { body } = document;
 
 function changeBackground(number) {
+    // Check if background is already showing
+    let previousBackground;
+    if (body.className) {
+        previousBackground = body.className;
+    }
+    // Reset CSS class for body
+    body.className = '';
     switch (number) {
         case '1':
-            body.classList.add('background-1');
-            break;
+            return (previousBackground === 'background-1' ? false : body.classList.add('background-1'));
         case '2':
-            body.classList.add('background-2');
-            break;
+            return (previousBackground === 'background-2' ? false : body.classList.add('background-2'));
         case '3':
-            body.classList.add('background-3');
-            break;
+            return (previousBackground === 'background-3' ? false : body.classList.add('background-3'));
         default:
             break;    
     }
